1ec67b5f13e253c73dea014f9b1f9d7b3c881c61
Sameeraprasad <138303912+Sameeraprasad@users.noreply.github.com>
2025-06-14 18:00:13 +0530

    Update HelloWorld.java

--- src/main/java/com/mt/sample/HelloWorld.java
+++ src/main/java/com/mt/sample/HelloWorld.java
@@ -1,4 +1,3 @@
-package com.mt.sample;
 
 /**
  * Hello world!
@@ -8,15 +7,16 @@ import java.util.Date;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		System.out.println("Hello Welcome to Maven Build Tool !! Today Date is:  " + getLocalCurrentDate());
+        public static void main(String[] args) {
+                System.out.println("Hello Welcome to Maven Build Tool !! Today Date is:  " + getLocalCurrentDate());
 
-	}
+        }
 
-	private static Date getLocalCurrentDate() {
-		String m = "Mithun Technologies";
-		return new Date();
+        private static Date getLocalCurrentDate() {
+                String m = "Mithun Technologies";
+                return new Date();
 
-	}
+        }
 
 }
+
